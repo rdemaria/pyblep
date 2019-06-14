87eafcf661165c2b7949d01423abbb904eada0fd
Riccardo De Maria <riccardodemaria@gmail.com>
2019-06-14 17:48:03 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='pyblep',
+        version='0.0.0',
+        description='Python Beam Line Exchange Protocol',
+        author='Riccardo De Maria',
+        author_email='riccardo.de.maria@cern.ch',
+        url='https://github.com/rdemaria/pyblep',
+        packages=find_packages(),
+        install_requires=['numpy'],
+)
+
+
