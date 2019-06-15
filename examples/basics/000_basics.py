383c47ede268de3fe6cdd1174e2efb57a392fb5c
giadarol <giovanni.iadarola@cern.ch>
2019-06-15 23:57:39 +0200

    Build a sixtracklib line from pblep line

--- examples/basics/000_basics.py
+++ examples/basics/000_basics.py
@@ -4,4 +4,8 @@ line, other_data = pyblep.from_sixtrack_input('sixtrack_input')
 
 # Build a pysixtrack line
 import pysixtrack
-psline = pysixtrack.Line.fromline(line)
+ps_line = pysixtrack.Line.fromline(line)
+
+# Build a pysixtracklib line
+import pysixtracklib
+pslib_line = pysixtracklib.Elements.fromline(line)
