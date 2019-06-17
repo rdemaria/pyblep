8fcba2e2dd91136d47c468f08fb3c422caf79d5d
giadarol <giovanni.iadarola@cern.ch>
2019-06-17 18:49:14 +0200

    Added tools

---
+++ pyblep/tools.py
@@ -0,0 +1,10 @@
+
+def get_elems_of_type(line, element_type):
+
+    pyst_ele_type_list = [ee.__class__.__name__ for ee in line.elements]
+    indlist = np.where([ss == element_type for ss in pyst_ele_type_list])[0]
+    ele_list = [line.elements[ind] for ind in indlist]
+    name_list = [line.elements[ind] for ind in indlist]
+
+    return indlist, name_list, ele_list
+
