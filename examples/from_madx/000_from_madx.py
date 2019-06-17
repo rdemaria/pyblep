3885ac182e8c6fa318dcc75a2686c00a7dcdec42
giadarol <giovanni.iadarola@cern.ch>
2019-06-17 11:54:09 +0200

    I manage to parse the entire sequence

--- examples/from_madx/000_from_madx.py
+++ examples/from_madx/000_from_madx.py
@@ -14,18 +14,23 @@ elements = seq.elements
 ele_pos = seq.element_positions()
 
 line = pbelms.Line(elements=[])
+element_names = []
 old_pp = 0.
+i_drift = 0
 for ee, pp in zip(elements, ele_pos):
     if pp>old_pp:
         line.elements.append(pbelms.Drift(length=(pp-old_pp)))
-    
+        element_names.append('drift_%d'%i_drift)
+        i_drift +=1 
+
     if ee.length > 0:
         raise ValueError(f"Seq {seq} contails {el} with length>0")
 
     mad_etype = ee.base_type.name
-    
+    eename = ee.name
+
     if mad_etype in ['marker', 'monitor', 'hmonitor', 'vmonitor',
-            'rcollimator', 'placeholder', 'instrument']:
+            'rcollimator', 'placeholder', 'instrument', 'solenoid']:
         newele = pbelms.Drift(length=0.)
 
     elif mad_etype == 'multipole':
@@ -55,7 +60,59 @@ for ee, pp in zip(elements, ele_pos):
     elif mad_etype == 'rfcavity':
         newele = pbelms.Cavity(voltage=ee.volt * 1e6,
                     frequency=ee.freq * 1e6, lag=ee.lag * 360)
+
+    elif mad_etype == 'beambeam':
+        if eename.startswith('bb_par'):
+            ## BB interaction is 4D
+            newele = pbelms.BeamBeam4D(
+                charge  = 0.,
+                sigma_x = 1.,
+                sigma_y = 1., 
+                beta_r  = 1., 
+                x_bb    = 0.,
+                y_bb    = 0., 
+                d_px    = 0.,
+                d_py    = 0.,
+                )
+
+        elif eename.startswith('bb_ho'):
+            ## BB interaction is 6D
+            newele = pbelms.BeamBeam6D(
+                phi     = 0., 
+                alpha   = 0.,   
+                x_bb_co = 0.,   
+                y_bb_co = 0.,   
+                charge_slices = [0.], 
+                zeta_slices = [0.],
+                sigma_11 = 1.,  
+                sigma_12 = 0.,  
+                sigma_13 = 0.,  
+                sigma_14 = 0.,  
+                sigma_22 = 1.,  
+                sigma_23 = 0.,  
+                sigma_24 = 0.,  
+                sigma_33 = 0.,  
+                sigma_34 = 0.,  
+                sigma_44 = 0.,  
+                x_co     = 0.,  
+                px_co    = 0.,  
+                y_co     = 0.,  
+                py_co    = 0.,  
+                zeta_co  = 0.,  
+                delta_co = 0.,  
+                d_x      = 0.,  
+                d_px     = 0.,  
+                d_y      = 0.,  
+                d_py     = 0.,  
+                d_zeta   = 0.,  
+                d_delta  = 0.,  
+                )
+        else:
+            raise ValueError(
+                    'Beam beam type for element "%s" not recognized')
+
     else:
         raise ValueError('Not recognized')
 
     line.elements.append(newele)
+    element_names.append(eename)
