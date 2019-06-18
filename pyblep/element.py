94033de0f66514a70b6facb7c7c6b6b05fc07b03
Riccardo De Maria <riccardodemaria@gmail.com>
2019-06-18 09:35:24 +0200

    dataclasses

--- pyblep/element.py
+++ pyblep/element.py
@@ -6,6 +6,7 @@ Conventions:
 """
 
 from collections import namedtuple
+from dataclasses import dataclass
 
 
 class _MetaElement(type):
@@ -28,6 +29,29 @@ class _MetaElement(type):
         # print("named",nt,nt._fields)
         return super(_MetaElement, cls).__new__(cls, clsname, (nt,), dct)
 
+class _MetaElement2(type):
+    def __new__(cls, clsname, bases, dct):
+        # print('-----------------------------------')
+        # print("Allocating memory for class", clsname)
+        # print(clsname)
+        # print(bases)
+        # print(dct)
+        description = dct.get('_description', {})
+        base=type(clsname,(object,),{})
+        base.__annotations__={}
+        for name,unit,desc in description:
+            base.__annotations__[name]=object
+        base=dataclass(base)
+        try:
+            doc = [dct['__doc__'], '\nFields:\n']
+        except KeyError:
+            doc = ['\nFields:\n']
+        fields = [f"{field:10} [{unit+']:':5} {desc} " for field,
+                  unit, desc in description]
+        doc += fields
+        dct['__doc__'] = "\n".join(doc)
+        # print("named",nt,nt._fields)
+        return super(_MetaElement2, cls).__new__(cls, clsname, (base,), dct)
 
-class Element(metaclass=_MetaElement):
+class Element(metaclass=_MetaElement2):
     pass
